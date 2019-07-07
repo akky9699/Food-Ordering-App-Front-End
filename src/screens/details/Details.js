fcd6af8db4cf1e427882f195e93b59f56d4ff1b3
deshpandesoniya16 <deshpandesoniya16@gmail.com>
2019-07-07 19:50:36 +0530

    Updated Details.js and Included Details.css in screens/details folder
    The details page allows customers to view the restaurant details and its menu items. On this page, a customer can also add the menu items in the cart and change the quantity of these menu items in the cart.
    1)The details page is displayed when a customer clicks on a card displaying a restaurant’s information on the home page.
    2)The following API endpoint, which we created inside RestaurantController in your back-end project, is used to get the entire information on this page about the restaurant based on its ID:
    /api/restaurant/{restaurantID}
    -Restaurant Information is displayed:
    1)Restaurant Name
    2)Restaurant Locality
    3)Categories
    4)Rating
    5)Number of customers who rated
    6)Average cost for two people to dine
    7)Average cost for two people to dine as text

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -2,18 +2,142 @@ import React, { Component } from "react";
 import "./Details.css";
 import { withStyles } from "@material-ui/core/styles";
 import Header from "../../common/header/Header";
-
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import Badge from "@material-ui/core/Badge";
+import ShoppingCart from "@material-ui/icons/ShoppingCart";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
 const styles = theme => ({});
 
 class Details extends Component {
+  constructor() {
+    super();
+    this.state = {
+      open: false,
+      btnClicked: "",
+      checkoutArr: [],
+      itemAdded: 0,
+      totalPrice: 0,
+      restaurantDetails: {
+        address: "",
+        average_price: "",
+        categories: "",
+        customer_rating: "",
+        id: "",
+        number_customers_rated: "",
+        photo_URL: "",
+        restaurant_name: ""
+      }
+    };
+  }
+
+  componentWillMount() {
+    // clear existing cart
+    sessionStorage.removeItem("customer-cart");
+
+    let that = this;
+    let dataRestaurant = null;
+    let xhrRestaurant = new XMLHttpRequest();
+    xhrRestaurant.addEventListener("readystatechange", function() {
+      if (this.readyState === 4) {
+        that.setState({
+          restaurantDetails: JSON.parse(this.responseText)
+        });
+      }
+    });
+    xhrRestaurant.open(
+      "GET",
+      `${this.props.baseUrl}/restaurant/${this.props.match.params.restaurantID}`
+    );
+    xhrRestaurant.setRequestHeader("Cache-Control", "no-cache");
+    xhrRestaurant.send(dataRestaurant);
+  }
+
+  getCategory = () => {
+    let data = this.state.restaurantDetails;
+    let dataLength = data.categories && data.categories.length;
+    return dataLength > 0
+      ? data.categories.map((item, index) => {
+          return (
+            <span key={index}>
+              {item.category_name}
+              {dataLength === index + 1 ? "" : ", "}{" "}
+            </span>
+          );
+        })
+      : null;
+  };
 
   render() {
-
+    const {
+      photo_URL,
+      restaurant_name,
+      address,
+      customer_rating,
+      average_price,
+      number_customers_rated
+    } = this.state.restaurantDetails;
 
     return (
       <div>
         <Header />
-     
+        <div>
+          <Grid container spacing={24} className="bggrey mobile-text-center">
+            <Grid item xs={12} sm={3} className="text-center">
+              <img
+                src={photo_URL}
+                width="300"
+                alt={photo_URL}
+                height="250"
+                className="margin-top-20"
+              />
+            </Grid>
+            <Grid item xs={12} sm={9}>
+              <Grid container spacing={24}>
+                <Grid item xs={12} sm={12} className="font-family-serif">
+                  <h1>{restaurant_name}</h1>
+                  <h3 style={{ textTransform: "uppercase" }}>
+                    {address.locality}
+                  </h3>
+                  <h3>{this.getCategory()}</h3>
+                  <Grid container spacing={24}>
+                    <Grid item xs={6} sm={6}>
+                      <div className="container_item3">
+                        <i className="fa fa-star" /> {customer_rating}
+                        <p className="text_format">
+                          AVERAGE RATING BY
+                          <br />
+                          <span className="bold">
+                            {number_customers_rated}
+                          </span>{" "}
+                          CUSTOMERS
+                        </p>
+                      </div>
+                    </Grid>
+                    <Grid item xs={6} sm={6}>
+                      <div className="container_item3">
+                        <i className="fa fa-inr" /> {average_price}
+                        <p className="text_format">
+                          AVERAGE COST FOR
+                          <br />
+                          TWO PEOPLE
+                        </p>
+                      </div>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          
+        </div>
       </div>
     );
   }
