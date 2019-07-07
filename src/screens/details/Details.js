f045e402019a0b236ff65dd27ff149fed68f4691
deshpandesoniya16 <deshpandesoniya16@gmail.com>
2019-07-07 20:05:30 +0530

    Updated Details.js and Details.css
    1)Created menu-items’ section on the DETAILS page.
    2)This section consists of all the category names that the restaurant serves and under each category, its menu items are displayed.
    3)Each row for the menu item is designed as per upgrad guidelines:-
    -Tag for veg./non-veg.
    -Name of menu-item
    -Price per unit
    -Add button

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -5,14 +5,7 @@ import Header from "../../common/header/Header";
 import Divider from "@material-ui/core/Divider";
 import IconButton from "@material-ui/core/IconButton";
 import AddIcon from "@material-ui/icons/Add";
-import Snackbar from "@material-ui/core/Snackbar";
-import CloseIcon from "@material-ui/icons/Close";
-import Badge from "@material-ui/core/Badge";
-import ShoppingCart from "@material-ui/icons/ShoppingCart";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
+
 import Grid from "@material-ui/core/Grid";
 const styles = theme => ({});
 
@@ -75,6 +68,96 @@ class Details extends Component {
       : null;
   };
 
+  getEachDish = (item_list, category_name) => {
+    return item_list.map((item, index) => {
+      item = { ...item, category_name: category_name };
+      return (
+        <div className="flex pd-1-per" key={index}>
+          <div className="flex-5">
+            <i
+              className={
+                item.item_type === "NON_VEG"
+                  ? "fa fa-circle non-veg"
+                  : "fa fa-circle veg"
+              }
+            />
+          </div>
+          <div className="flex-75">{item.item_name}</div>
+          <div className="flex-10">
+            <i className="fa fa-inr" /> {item.price}.00
+          </div>
+          <div className="flex-10 plus-btn">
+            <IconButton
+              aria-label="Add"
+              style={{ padding: "1px" }}
+              onClick={this.addMenuClick(item, "ADD")}
+            >
+              <AddIcon />
+            </IconButton>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  
+
+  getCategoryList = () => {
+    let data = this.state.restaurantDetails;
+    let dataLength = data.categories && data.categories.length;
+    return dataLength > 0
+      ? data.categories.map((item, index) => {
+          return (
+            <div className="mt-15" key={"item" + item.id}>
+              <div>{item.category_name}</div>
+              <Divider className="divider-margin-10" />
+              {this.getEachDish(item.item_list, item.category_name)}
+            </div>
+          );
+        })
+      : null;
+  };
+
+  addMenuClick = (item, method) => event => {
+    let selectedItem, newAdded;
+    let duplicates = this.state.checkoutArr.filter(
+      data => item.id === data.id && item.category_name === data.category_name
+    );
+    if (duplicates.length > 0) {
+      selectedItem = this.state.checkoutArr.map(eachItem => {
+        if (
+          eachItem.id === duplicates[0].id &&
+          eachItem.category_name === duplicates[0].category_name
+        ) {
+          let count = eachItem.count + 1;
+          eachItem.count = count;
+          eachItem.totalItemPrice = eachItem.price * count;
+        }
+        return eachItem;
+      });
+      newAdded = [...selectedItem];
+    } else {
+      let count = duplicates.length + 1;
+      selectedItem = {
+        ...item,
+        count: count,
+        totalItemPrice: item.price * count
+      };
+      newAdded = [...this.state.checkoutArr, selectedItem];
+    }
+
+    const itemLength = this.state.itemAdded + 1;
+    const totalPrice = this.state.totalPrice + item.price;
+    this.setState({
+      checkoutArr: newAdded,
+      open: true,
+      btnClicked: method,
+      itemAdded: itemLength,
+      totalPrice: totalPrice
+    });
+  };
+
+
   render() {
     const {
       photo_URL,
@@ -136,7 +219,13 @@ class Details extends Component {
               </Grid>
             </Grid>
           </Grid>
-          
+          <Grid container spacing={24} className="item-list-container">
+            <Grid item xs={12} sm={7}>
+              {this.getCategoryList()}
+            </Grid>
+         
+          </Grid>
+         
         </div>
       </div>
     );
